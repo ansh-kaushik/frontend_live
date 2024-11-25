51bcf3e98369a30ac66781df847c4c647c0da949
ANSH KAUSHIK <anshkaushik9454@gmail.com>
2024-11-25 13:27:20 +0530

    fixes

--- src/LiveStream.jsx
+++ src/LiveStream.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://backend-live-sr7w.onrender.com:3000", {
   transports: ["websocket"], // Ensure WebSocket is used
 });
 
